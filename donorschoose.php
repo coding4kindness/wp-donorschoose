0f3456433c486d4021d9b7f4538c1f2cbf2b2965
Andrew Roden <aroden@salesforce.com>
2015-07-20 17:37:29 -0400

    curl to file_get_contents

--- donorschoose.php
+++ donorschoose.php
@@ -26,12 +26,16 @@ function curlGetContent($baseUrl, $apiKey, $filters)
 
 	$url = $baseUrl . "?" . $apiKeyQs . "&" . $filtersQs;
 
+	/*
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_HEADER, false);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_GETT, true);
 	$rawData = curl_exec($curl);
+	curl_exec($curl);
+	*/
+	$rawData = file_get_contents($url);
 
 	$json = json_decode($rawData, true);
 	return $json;
