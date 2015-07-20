9458ceb88d7f35824f33b0ae9471c70e3857f44b
Andrew Roden <aroden@salesforce.com>
2015-07-20 13:38:18 -0400

    Plugin header metadoc

---
+++ donorschoose.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Donors Choose
+Plugin URI:  https://github.com/coding4kindness/wp-donorschoose
+Description: Plugin for donors choose campaigns
+Version:     0.1
+Author:      Andrew Roden, Freeman Parks
+License:     Apache 2.0
+License URI: http://www.apache.org/licenses/
+*/
+>
+
